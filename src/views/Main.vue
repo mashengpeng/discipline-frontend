08bab647bcf9ed06d8aa0f400545a63104da6d8b
shengpeng.ma <shengpeng.ma@nio.com>
2023-06-19 18:43:17 +0800

    main.vue样式调整

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,12 +1,15 @@
-<script setup>
-
-</script>
-
 <template>
-header
-  <router-view></router-view>
+  <a-layout>
+    <a-layout-header class="bg-amber-50">Header</a-layout-header>
+    <a-layout class="flex">
+      <a-layout-sider class="bg-amber-50">Sider</a-layout-sider>
+      <a-layout-content class="h-screen"><router-view></router-view></a-layout-content>
+    </a-layout>
+    <a-layout-footer>Footer</a-layout-footer>
+  </a-layout>
 </template>
-
-<style scoped>
+<script>
+</script>
+<style>
 
 </style>
