e5ad9658a03102c4e7449fd881a1f70ed31618be
mashengpeng <1360311705@qq.com>
2023-07-02 21:20:50 +0800

    默认tab

--- src/router/index.js
+++ src/router/index.js
@@ -1,74 +1,89 @@
 // 全局路由(无需嵌套上左右整体布局)
-import {createRouter, createWebHistory} from "vue-router";
-import http from "@/utils/http";
-import {ElNotification} from 'element-plus'
+import { createRouter, createWebHistory } from 'vue-router';
+import http from '@/utils/http';
+import { ElNotification } from 'element-plus';
 
 const globalRoutes = [
-    {path: '/404', component: () => import('@/views/404.vue'), name: '404', meta: {title: '404未找到', anno: true}},
-    {path: '/login', component: () => import('@/views/Login.vue'), name: 'login', meta: {title: '登录', anno: true}}
-]
+  { path: '/404', component: () => import('@/views/404.vue'), name: '404', meta: { title: '404未找到', anno: true } },
+  {
+    path: '/login',
+    component: () => import('@/views/Login.vue'),
+    name: 'login',
+    meta: { title: '登录', anno: true },
+  },
+];
 
 // 主入口路由(需嵌套上左右整体布局)
 const mainRoutes = {
-    path: '/',
-    component: () => import('@/views/Main.vue'),
-    // name: 'main',
-    children: [
-        {path: '/', component: () => import('@/views/Home.vue'), name: 'home', meta: {title: '首页'}},
-        {path: '/todo', component: () => import('@/views/Todo.vue'), name: 'todo', meta: {title: '代办事项'}},
-        {
-            path: '/article/list',
-            component: () => import('@/views/ArticleList.vue'),
-            name: 'articleList',
-            meta: {title: '文章'}
-        },
-        {path: '/article/:id', component: () => import('@/views/Article.vue'), name: 'article', meta: {title: '文章'}},
-        {path: '/question', component: () => import('@/views/Question.vue'), name: 'question', meta: {title: '题目'}},
-    ],
-}
+  path: '/',
+  component: () => import('@/views/Main.vue'),
+  // name: 'main',
+  children: [
+    { path: '/', component: () => import('@/views/Home.vue'), name: 'home', meta: { title: '首页' } },
+    { path: '/todo', component: () => import('@/views/Todo.vue'), name: 'todo', meta: { title: '代办事项' } },
+    {
+      path: '/article',
+      component: () => import('@/views/ArticleList.vue'),
+      name: 'articleList',
+      meta: { title: '文章' },
+    },
+    {
+      path: '/article/:id',
+      component: () => import('@/views/Article.vue'),
+      name: 'article',
+      meta: { title: '文章' },
+    },
+    {
+      path: '/question',
+      component: () => import('@/views/Question.vue'),
+      name: 'question',
+      meta: { title: '题目' },
+    },
+  ],
+};
 
 export const router = createRouter({
-    // 创建一个 hash 历史记录。
-    history: createWebHistory(),
-    // 应该添加到路由的初始路由列表。
-    routes: globalRoutes.concat(mainRoutes),
-    //routes: mainRoutes,
-    // 是否应该禁止尾部斜杠。默认为假
-    strict: true,
-    scrollBehavior: () => ({left: 0, top: 0}),
+  // 创建一个 hash 历史记录。
+  history: createWebHistory(),
+  // 应该添加到路由的初始路由列表。
+  routes: globalRoutes.concat(mainRoutes),
+  //routes: mainRoutes,
+  // 是否应该禁止尾部斜杠。默认为假
+  strict: true,
+  scrollBehavior: () => ({ left: 0, top: 0 }),
 });
 
 router.beforeEach((to, from, next) => {
 
-    document.title = to.meta.title ?? "vue";
+  document.title = to.meta.title ?? 'vue';
 
-    if (to.meta.anno) {
-        next()
-        return
-    }
-    // ElNotification({
-    //     title: '检查登录',
-    //     type: 'info',
-    //     offset: 50,
-    // });
-    http.get("/user/isLogin").then(
-        (res) => {
-            if (!res.data) {
-                ElNotification({
-                    title: '未登录,请先登录',
-                    type: 'info',
-                    duration: 1000
-                });
-                localStorage.clear()
-                next({name: 'login'})
-            } else {
-                next()
-            }
-        },
-        () => {
-            next({name: 'login'})
-        }
-    );
+  if (to.meta.anno) {
+    next();
+    return;
+  }
+  // ElNotification({
+  //     title: '检查登录',
+  //     type: 'info',
+  //     offset: 50,
+  // });
+  http.get('/user/isLogin').then(
+    (res) => {
+      if (!res.data) {
+        ElNotification({
+          title: '未登录,请先登录',
+          type: 'info',
+          duration: 1000,
+        });
+        localStorage.clear();
+        next({ name: 'login' });
+      } else {
+        next();
+      }
+    },
+    () => {
+      next({ name: 'login' });
+    },
+  );
 });
 
 
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,39 +1,39 @@
 <template>
   <el-container>
-    <el-header class="flex justify-center p-0 h-[50px]">
-      <el-menu mode="horizontal"
+    <el-header class='flex justify-center p-0 h-[50px]'>
+      <el-menu :default-active='activeMenu()'
+               :ellipsis='false'
+               class='w-full sm:pl-16 sm:pr-16 md:pl-32 md:pr-32 lg:pl-48 lg:pr-48  xl:pl-64 xl:pr-64 2xl:pl-96 2xl:pr-96'
+               mode='horizontal'
                router
-               :ellipsis="false"
-               class="w-full sm:pl-16 sm:pr-16 md:pl-32 md:pr-32 lg:pl-48 lg:pr-48  xl:pl-64 xl:pr-64 2xl:pl-96 2xl:pr-96"
-               :default-active="route.path"
       >
-        <el-menu-item index="/" class="flex-1">首页</el-menu-item>
-        <el-menu-item index="/todo" class="flex-1">代办事项</el-menu-item>
-        <el-menu-item index="/article/list" class="flex-1">文章</el-menu-item>
-        <el-menu-item index="/question" class="flex-1">题目</el-menu-item>
-        <el-menu-item index="/login" class="flex-1" @click="logout">注销</el-menu-item>
+        <el-menu-item class='flex-1' index='/'>首页</el-menu-item>
+        <el-menu-item class='flex-1' index='/todo'>代办事项</el-menu-item>
+        <el-menu-item class='flex-1' index='/article'>文章</el-menu-item>
+        <el-menu-item class='flex-1' index='/question'>题目</el-menu-item>
+        <el-menu-item class='flex-1' index='/login' @click='logout'>注销</el-menu-item>
       </el-menu>
     </el-header>
-    <el-main class="p-0 h-[calc(100vh-100px)]">
+    <el-main class='p-0 h-[calc(100vh-100px)]'>
       <el-scrollbar>
         <div
-            class="sm:ml-16 sm:mr-16 md:ml-32 md:mr-32 lg:ml-48 lg:mr-48  xl:ml-64 xl:mr-64 2xl:ml-96 2xl:mr-96 sm:pl-32 sm:pr-32 sm:pt-4 sm:pb-4 min-h-[calc(100vh-100px)] lg:shadow-2xl">
-          <el-button size="large"
-                     class="flex-1 border-0 shadow fixed z-50 left-[4px] sm:left-[6rem] md:left-[10rem] lg:left-[14rem] xl:left-[18rem] 2xl:left-[26rem] bottom-16 sm:top-16"
-                     @click="router.back()" circle>
-            <el-icon size="30">
-              <caret-left/>
+          class='sm:ml-16 sm:mr-16 md:ml-32 md:mr-32 lg:ml-48 lg:mr-48  xl:ml-64 xl:mr-64 2xl:ml-96 2xl:mr-96 sm:pl-32 sm:pr-32 sm:pt-4 sm:pb-4 min-h-[calc(100vh-100px)] lg:shadow-2xl'>
+          <el-button circle
+                     class='flex-1 border-0 shadow fixed z-50 left-[4px] sm:left-[6rem] md:left-[10rem] lg:left-[14rem] xl:left-[18rem] 2xl:left-[26rem] bottom-16 sm:top-16'
+                     size='large' @click='router.back()'>
+            <el-icon size='30'>
+              <caret-left />
             </el-icon>
           </el-button>
           <router-view></router-view>
         </div>
-        <el-backtop :right="40" :bottom="80" target=".el-scrollbar__wrap"/>
+        <el-backtop :bottom='80' :right='40' target='.el-scrollbar__wrap' />
       </el-scrollbar>
     </el-main>
-    <el-footer class="p-0 h-[50px] flex justify-center bg-white border">
+    <el-footer class='p-0 h-[50px] flex justify-center bg-white border'>
       <el-space>
-        <el-link href="https://github.com/" target="_blank">github</el-link>
-        <el-link href="https://gitlab.com/" target="_blank">gitlab</el-link>
+        <el-link href='https://github.com/' target='_blank'>github</el-link>
+        <el-link href='https://gitlab.com/' target='_blank'>gitlab</el-link>
       </el-space>
 
     </el-footer>
@@ -41,27 +41,32 @@
 
 </template>
 <script setup>
-import {useRoute, useRouter} from "vue-router";
-import http from '@/utils/http'
-import {ElNotification} from 'element-plus'
-import {CaretLeft} from "@element-plus/icons-vue";
+import { useRoute, useRouter } from 'vue-router';
+import http from '@/utils/http';
+import { ElNotification } from 'element-plus';
+import { CaretLeft } from '@element-plus/icons-vue';
 
 const router = useRouter();
 const route = useRoute();
 
+
+const activeMenu = () => {
+  console.log(route.path.split('/').slice(0, 2).join('/'));
+  return route.path.split('/').slice(0, 2).join('/');
+};
 const logout = () => {
-  http.get("/user/logout").then(
-      (res) => {
-        ElNotification({
-          title: '注销成功',
-          type: 'info',
-          duration: 1000
-        });
-      },
-      () => {
-      }
+  http.get('/user/logout').then(
+    (res) => {
+      ElNotification({
+        title: '注销成功',
+        type: 'info',
+        duration: 1000,
+      });
+    },
+    () => {
+    },
   );
-}
+};
 
 </script>
 <style scoped>
