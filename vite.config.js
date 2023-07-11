68b0cccd4de90fd03ef3f4c7923fea9738ae7e4b
mashengpeng <1360311705@qq.com>
2023-07-11 21:12:15 +0800

    多环境

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,6 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  define: {
-    'process.env': {
-      VUE_APP_URL: 'http://localhost:8888',
-    },
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
